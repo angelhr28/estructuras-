3d4eea20c4106ee3e8816b02d893d542b6b40a81
angelhr28 <43622761+angelhr28@users.noreply.github.com>
2018-09-27 17:12:58 -0500

    Add files via upload

--- p5a.cpp
+++ p5a.cpp
@@ -3,42 +3,46 @@
 #include <stdio.h>
 #include <conio.h>
 using namespace std; 
-int cantidad;
 struct nombres{
 	string alumno;
 	int codigo;
 	int promedio;
 	int edad; 
 };
+void datos(int);
+void futuro(int);
 
+
+nombres *persona;
 
 int main (){
-
-cout<<"ingrese la cantidad de personas ";
-cin>>cantidad;
-nombres persona[cantidad];
-for(int i=0; i<cantidad;i++){
-
-
-
-cout<<"ingrese el nombre: "; 
-fflush(stdin); 
-getline(cin,persona[i].alumno);// al momento de poner el punto se almacena en la variable creada.Xd
-cout<<"ingrese el codigo : "<<endl;
-cin>>persona[i].codigo;
-cout<<"ingrese la edad : "<<endl;
-cin>>persona[i].edad;
-cout<<"ingrese el promedio : "<<endl;
-cin>>persona[i].promedio;
-
+	int cantidad;
+	cout<<"ingrese la cantidad de personas ";
+	cin>>cantidad;
+	persona=new nombres[cantidad];
+	datos(cantidad);
+	futuro(cantidad);
+	return 0;
 }
 
+void datos(int x){
 
-for(int i=0; i<cantidad;i++){
-
-
-
-cout<<"la edad dentro de 20 años de  "<<persona[i].alumno<<"	es	"<<(persona[i].edad)+20<<endl;
+	for(int i=0; i<x;i++){
+		cout<<"ingrese el nombre: "; 
+		fflush(stdin); 
+		getline(cin,persona[i].alumno);// al momento de poner el punto se almacena en la variable creada.Xd
+		cout<<"ingrese el codigo : "<<endl;
+		cin>>persona[i].codigo;
+		cout<<"ingrese la edad : "<<endl;
+		cin>>persona[i].edad;
+		cout<<"ingrese el promedio : "<<endl;
+		cin>>persona[i].promedio;
+	
+	}
 }
-return 0;
+
+void futuro(int x){
+	for(int i=0; i<x;i++){
+		cout<<"la edad dentro de 20 años de  "<<persona[i].alumno<<"	es	"<<(persona[i].edad)+20<<endl;
+	}
 } 

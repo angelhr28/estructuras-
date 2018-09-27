c33354ebbfec259e8d8d9cec143637b1876088c5
angelhr28 <43622761+angelhr28@users.noreply.github.com>
2018-09-27 17:22:30 -0500

    lo de l profe

---
+++ fucionesConVectores.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+//crearmos nuestra estrcutura
+struct Datos{
+	int dni;
+	string nombre;
+	int edad;
+	
+};
+void llenar(Datos estudiante[]);
+void mostrar(Datos estudiante[]);
+
+int main(){
+	Datos *alumno=new Datos[20];
+	llenar(alumno);
+	mostrar(alumno);
+	
+}
+
+void llenar(Datos estudiante[]){
+	
+
+	cout<<"ingrese los datos: "<<endl;
+	for(int i=1;i<=2;i++){
+		cout<<"ingrese el dni: ";
+		cin>>estudiante[i].dni;
+		cout<<"ingrese el nombre es: ";
+		fflush(stdin);
+		getline(cin,estudiante[i].nombre);
+		cout<<"ingrese su edad: ";
+		cin>>estudiante[i].edad;
+	}
+}
+void mostrar(Datos estudiante[]){
+	
+
+	cout<<"ingrese los datos: "<<endl;
+	for(int i=1;i<=2;i++){
+		cout<<"su dni es  ";
+		cout<<estudiante[i].dni<<endl;
+		cout<<"su nombre es: ";
+		
+		cout<<estudiante[i].nombre<<endl;
+		cout<<"su su edad: ";
+		cout<<estudiante[i].edad;
+	}
+}
+
+	
+
